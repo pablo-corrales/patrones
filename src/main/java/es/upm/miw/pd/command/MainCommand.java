7ac4e37562b3b76a8ef655424c1b5d64c6dd2417
miw-upm <miw@eui.upm.es>
2014-10-07 07:42:27 +0200

    command

--- src/main/java/es/upm/miw/pd/command/MainCommand.java
+++ src/main/java/es/upm/miw/pd/command/MainCommand.java
@@ -3,28 +3,18 @@ package es.upm.miw.pd.command;
 import upm.jbb.IO;
 
 public final class MainCommand {
-    private Orden orden1, orden2;
+    private Invocador invocador;
 
     public MainCommand() {
         Receptor receptor = new Receptor();
-        this.orden1 = new OrdenConcreta1(receptor);
-        this.orden2 = new OrdenConcreta2(receptor);
+        this.invocador= new Invocador();
+        this.invocador.añadir(new OrdenConcreta1(receptor));
+        this.invocador.añadir(new OrdenConcreta2(receptor));
     }
 
     public void invocador() {
-        Orden[] ordenes = new Orden[2];
-        ordenes[0] = this.orden1;
-        ordenes[1] = this.orden2;
-        Orden orden = (Orden) IO.in.select(ordenes);
-        orden.ejecutar();
-    }
-
-    public void invocador1() {
-        this.orden1.ejecutar();
-    }
-
-    public void invocador2() {
-        this.orden2.ejecutar();
+        String llave = (String) IO.in.select(this.invocador.llaves());
+        this.invocador.invocar(llave);
     }
 
     public static void main(String[] args) {
