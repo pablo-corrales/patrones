2a80537298ea9cc1be277a483a3535470c2ae801
pablo-corrales <charliequiro>
2014-10-14 21:42:03 +0200

    Diseño con polimorfismo con adicion y sin multiplicacion

--- src/main/java/es/upm/miw/pd/doo/polymorphism/inheritance/lackOf/Main.java
+++ src/main/java/es/upm/miw/pd/doo/polymorphism/inheritance/lackOf/Main.java
@@ -1,20 +1,17 @@
 package es.upm.miw.pd.doo.polymorphism.inheritance.lackOf;
 
+
 public class Main {
 
-    public static void main(String[] args) {
-        Operations operations = new Operations();
-        operations.add(new Addition(3, 4));
+public static void main(String[] args){
+		
+		OperationsHandler op = new OperationsHandler();
+				
+		op.ejecuta(new Addition(1,2));
+		op.ejecuta(new Substraction(4,3));
+				
+		
+	}
 
-        System.out.println("Total1:" + operations.total());
-
-        operations.add(new Subtraction(1, 1));
-        operations.add(new Addition(3, 5));
-        operations.add(new Subtraction(4, 1));
-        operations.add(new Addition(3, 6));
-        operations.add(new Subtraction(3, 2));
-
-        System.out.println("Total2:" + operations.total());
-    }
-
+   
 }
---
+++ src/main/java/es/upm/miw/pd/doo/polymorphism/inheritance/lackOf/OperationsHandler.java
@@ -0,0 +1,16 @@
+package es.upm.miw.pd.doo.polymorphism.inheritance.lackOf;
+
+public class OperationsHandler {
+	
+	
+	public OperationsHandler(){
+			
+	}
+	
+	public void ejecuta(Operations operacion) {
+				
+	}
+	
+	
+	
+}
