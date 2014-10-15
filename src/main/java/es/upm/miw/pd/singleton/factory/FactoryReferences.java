998248076dc9ac9e7d5b43c88b256fdc7c77cd22
pablo-corrales <charliequiro>
2014-10-15 23:26:57 +0200

    Clase FactoryReferences refactorizado respetando funcionalidad original

--- src/main/java/es/upm/miw/pd/singleton/factory/FactoryReferences.java
+++ src/main/java/es/upm/miw/pd/singleton/factory/FactoryReferences.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FactoryReferences {
+	private static FactoryReferences factoryReferences;
     private Map<String, Integer> references;
 
     private int reference;
@@ -13,6 +14,13 @@ public class FactoryReferences {
         this.reference = 0;
     }
 
+    public static FactoryReferences getFactory(){
+    	if (factoryReferences == null){
+    		factoryReferences = new FactoryReferences(); 
+    	}
+    	return factoryReferences;
+    	
+    }
     public int getReference(String key) {
         Integer result = this.references.get(key);
         if (result == null) {
@@ -22,6 +30,8 @@ public class FactoryReferences {
         }
         return result;
     }
+    
+    
 
     public void removeReference(String key) {
         this.references.remove(key);
