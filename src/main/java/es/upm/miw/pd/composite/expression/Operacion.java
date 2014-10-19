3783cdeaf210eb66b0487826bd3ae4a86d76ea91
pablo-corrales <charliequiro>
2014-10-19 13:15:50 +0200

    Se crea clase Operacion

---
+++ src/main/java/es/upm/miw/pd/composite/expression/Operacion.java
@@ -0,0 +1,29 @@
+package es.upm.miw.pd.composite.expression;
+
+import es.upm.miw.pd.composite.expression.Expresion;
+
+public abstract class Operacion extends Expresion {
+    private Expresion operando1;
+    private Expresion operando2;
+    private char operador;
+
+   
+    public Operacion(Expresion operando1, char operador, Expresion operando2) {
+        this.operando1=operando1;
+        this.operador = operador;
+        this.operando2=operando2;
+        
+    }
+    
+    
+
+    @Override
+    protected abstract int operar();
+    
+    
+    @Override
+    protected String toSring() {
+        return "(" + operando1.toSring() + operador + operando2.toSring() + ")";
+    }
+
+}
