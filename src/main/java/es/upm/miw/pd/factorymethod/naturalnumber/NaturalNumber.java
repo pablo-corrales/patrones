6b045cca13d00ebab6ef839a129449f17cfa1059
pablo-corrales <charliequiro>
2014-10-20 00:59:24 +0200

    Se crea clase abstracta NaturalNumber con los metodos genericos de NaturalNumberES y se refactoriza clase NaturalNumberES

---
+++ src/main/java/es/upm/miw/pd/factorymethod/naturalnumber/NaturalNumber.java
@@ -0,0 +1,34 @@
+package es.upm.miw.pd.factorymethod.naturalnumber;
+
+public abstract class NaturalNumber {
+	
+	protected int value;
+    protected String[] textValue;
+
+    public NaturalNumber(int value,String[] textValue){
+        this.value=value;
+        this.textValue=textValue;
+    }
+    
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        assert value >= 0;
+        this.value = value;
+    }
+
+    public void add(int value) {
+        this.setValue(this.value + value);
+    }
+
+    public String getTextValue() {
+        if (this.value < textValue.length) {
+            return textValue[this.value];
+        } else {
+            return "???";
+        }
+    }
+
+}
--- src/main/java/es/upm/miw/pd/factorymethod/naturalnumber/NaturalNumberEs.java
+++ src/main/java/es/upm/miw/pd/factorymethod/naturalnumber/NaturalNumberEs.java
@@ -1,32 +1,11 @@
 package es.upm.miw.pd.factorymethod.naturalnumber;
 
-public class NaturalNumberEs {
-    private int value;
+public class NaturalNumberEs extends NaturalNumber {
 
-    private static final String[] textValue = {"cero", "uno", "dos", "tres", "cuatro", "cinco"};
+    public static final String[] textValue = {"cero", "uno", "dos", "tres", "cuatro", "cinco"};
 
     public NaturalNumberEs(int value) {
-        this.setValue(value);
+        super(value,textValue);
     }
 
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        assert value >= 0;
-        this.value = value;
-    }
-
-    public void add(int value) {
-        this.setValue(this.value + value);
-    }
-
-    public String getTextValue() {
-        if (this.value < textValue.length) {
-            return NaturalNumberEs.textValue[this.value];
-        } else {
-            return "???";
-        }
-    }
 }
