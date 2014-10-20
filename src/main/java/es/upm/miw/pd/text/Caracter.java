60d587a60b940f14946578af805671082efcf21b
pablo-corrales <charliequiro>
2014-10-20 23:15:00 +0200

    Se crean clases vacías Caracter, FactoriaCaracter, Parrafo, Texto para posterior implementacion

---
+++ src/main/java/es/upm/miw/pd/text/Caracter.java
@@ -0,0 +1,23 @@
+package es.upm.miw.pd.text;
+
+public class Caracter extends Componente{
+
+	@Override
+	public void add(Componente componente) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String dibujar(Boolean mayusculas) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean isCompuesto() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/es/upm/miw/pd/text/Parrafo.java
@@ -0,0 +1,17 @@
+package es.upm.miw.pd.text;
+
+public class Parrafo extends Compuesto{
+
+	@Override
+	public void add(Componente componente) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String dibujar(Boolean mayusculas) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
